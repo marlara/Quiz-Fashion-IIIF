538dfdf3a1cf57a6d1ec09fac29762461cee8a34
Lara Marziali <lara.marzi@gmail.com>
2021-12-15 15:48:51 +0100

    home and header styling

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,16 +1,18 @@
 <template>
   <header>
-    <div id="nav">
-      <router-link id="logo" to="/">Home page</router-link>
-      <router-link 
-      v-for="quiz in quizzes"
-      :key="quiz.id"
-      :to="{name: 'quiz.show', params:{id: quiz.id}}" 
-    >
-    <!-- insert slug in params, see this lesson https://vueschool.io/lessons/named-routes -->
-    {{quiz.name}}
-    </router-link> 
-    </div>
+      <h1>1972. Moda, design, storia</h1>
+      <div class="router">
+        <a href="https://mostra1972.unipr.it">Torna alla mostra</a>
+        <router-link id="logo" to="/">Scegli il quiz</router-link>
+        <router-link 
+        v-for="quiz in quizzes"
+        :key="quiz.id"
+        :to="{name: 'quiz.show', params:{id: quiz.id}}" 
+      >
+      <!-- insert slug in params, see this lesson https://vueschool.io/lessons/named-routes -->
+      {{quiz.name}}
+      </router-link> 
+      </div>
   </header>
 </template>
 
--- views/Home.vue
+++ views/Home.vue
@@ -1,15 +1,28 @@
 <template>
-  <h1>Home Page</h1>
+  <div id="content">
+    
+    <div class="rectangle_title">
+      <h1 style="margin-left: 20px">Scegli il quiz</h1>
+    </div>
 
-  <div class="quizzes">
-    <router-link 
-      v-for="quiz in quizzes"
-      :key="quiz.id"
-      :to="{name: 'quiz.show', params:{id: quiz.id}}"
-    >
-    <h2>{{quiz.name}}</h2>
-    <img :src="`/images/${quiz.image}`" :alt="quiz.name">
-    </router-link>
+    <img class="home_image" src="@/assets/versace_logo_ritagliato_nero.png" />
+
+    <div class="homeDescription">
+      <p>Scegli uno dei quiz e rispondi alle domande. Ad ogni risposta corretta si comporrà un pezzo del puzzle. Se risponderai correttamente a tutto avrai la figura intera!</p>
+      <div class="quizzes">
+      <router-link 
+        v-for="quiz in quizzes"
+        :key="quiz.id"
+        :to="{name: 'quiz.show', params:{id: quiz.id}}"
+      >
+      <h2 class="highlight-quiz">{{quiz.name}}</h2>
+      </router-link>
+      </div>
+    </div>
+    <div class="clear"></div>
+
+
+    
   </div>
 
 </template>
